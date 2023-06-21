0008bc617f645f8b08c9ec5552ecfe5ae2aef4ef
Alex Reinhart <alex@refsmmat.com>
2023-06-21 16:05:35 -0400

    Code formatting

--- prepare.lua
+++ prepare.lua
@@ -2,7 +2,7 @@
 -- https://github.com/cscheid/quarto-example-project-file-metadata
 
 -- Open metadata file and truncate it, so we start fresh
-local f = io.open('metadata.json','w')
+local f = io.open('metadata.json', 'w')
 if f == nil then
     error("couldn't open file for writing")
 end
